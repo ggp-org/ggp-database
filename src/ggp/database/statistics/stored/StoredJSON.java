a4a0e7070d42b1ea231c9303d848388e4fa2d32e
Sam Schreiber <sam.schreiber@gmail.com>
2012-08-09 05:57:19 +0000

    Truncate doubles in stored JSON to two decimal places, to reduce the size of the stats datastore entries.

--- src/ggp/database/statistics/stored/StoredJSON.java
+++ src/ggp/database/statistics/stored/StoredJSON.java
@@ -1,5 +1,10 @@
 package ggp.database.statistics.stored;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeSet;
+
 import ggp.database.Persistence;
 import ggp.database.statistics.StringCompressor;
 
@@ -7,8 +12,11 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.annotations.*;
 
 import com.google.appengine.api.datastore.Text;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONString;
 
 @PersistenceCapable
 @Inheritance(strategy=InheritanceStrategy.SUBCLASS_TABLE)
@@ -34,7 +42,7 @@ public abstract class StoredJSON {
     }
     
     public void setJSON(JSONObject theJSON) {
-        theData = new Text(StringCompressor.compress(theJSON.toString()));
+        theData = new Text(StringCompressor.compress(truncateDoublesForJSON(theJSON)));
         save();
     }
     
@@ -42,5 +50,94 @@ public abstract class StoredJSON {
         PersistenceManager pm = Persistence.getPersistenceManager();
         pm.makePersistent(this);
         pm.close();        
+    }
+    
+    // What is this? Well... we store a compressed version of the
+    // string representation of the JSON. The string representation
+    // of the JSON is dominated by variable names (which compress
+    // well, since they're frequently repeated) and floating point
+    // values (with many decimal places, which don't compress well).
+    // Since that degree of precision really isn't necessary when
+    // computing average scores and such, this function is used to
+    // truncate the doubles that appear in the JSON to two decimal
+    // places, so they take less room to store. This is really a
+    // messy hack to avoid storing over 1MB of data in a single
+    // datastore entry.
+    static String truncateDoublesForJSON(Object x) {
+        try {
+            if (x instanceof JSONObject) {
+                JSONObject theObject = (JSONObject)x;
+                
+                // Sort the keys
+                TreeSet<String> t = new TreeSet<String>();
+                Iterator<?> i = theObject.keys();
+                while (i.hasNext()) t.add(i.next().toString());
+                Iterator<String> keys = t.iterator();
+                
+                StringBuffer sb = new StringBuffer("{");    
+                while (keys.hasNext()) {
+                    if (sb.length() > 1) {
+                        sb.append(',');
+                    }
+                    Object o = keys.next();
+                    sb.append(JSONObject.quote(o.toString()));
+                    sb.append(':');
+                    sb.append(truncateDoublesForJSON(theObject.get(o.toString())));
+                }
+                sb.append('}');
+                return sb.toString();
+            } else if (x instanceof JSONArray) {
+                JSONArray theArray = (JSONArray)x;
+                StringBuffer sb = new StringBuffer();
+                sb.append("[");
+                int len = theArray.length();
+                for (int i = 0; i < len; i += 1) {
+                    if (i > 0) {
+                        sb.append(",");
+                    }
+                    sb.append(truncateDoublesForJSON(theArray.get(i)));
+                }
+                sb.append("]");
+                return sb.toString();
+            } else {
+                if (x == null || x.equals(null)) {
+                    return "null";
+                }
+                if (x instanceof JSONString) {
+                    Object object;
+                    try {
+                        object = ((JSONString)x).toJSONString();
+                    } catch (Exception e) {
+                        throw new JSONException(e);
+                    }
+                    if (object instanceof String) {
+                        return (String)object;
+                    }
+                    throw new JSONException("Bad value from toJSONString: " + object);
+                }
+                if (x instanceof Number) {
+                	if (x instanceof Double) {
+                		x = Math.floor((Double)x*100)/100.0;
+                	}
+                    return JSONObject.numberToString((Number)x);
+                }
+                if (x instanceof Boolean || x instanceof JSONObject ||
+                        x instanceof JSONArray) {
+                    return x.toString();
+                }
+                if (x instanceof Map) {
+                    return truncateDoublesForJSON(new JSONObject((Map<?,?>)x)).toString();
+                }
+                if (x instanceof Collection) {
+                    return truncateDoublesForJSON(new JSONArray((Collection<?>)x)).toString();
+                }
+                if (x.getClass().isArray()) {
+                    return truncateDoublesForJSON(new JSONArray(x)).toString();
+                }
+                return JSONObject.quote(x.toString());
+            }
+        } catch (Exception e) {
+            return null;
+        }            
     }
 }
