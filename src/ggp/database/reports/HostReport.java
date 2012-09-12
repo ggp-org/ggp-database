1fe2fecbc18ec469ae3d8c8b074bd309f17df61a
Sam Schreiber <sam.schreiber@gmail.com>
2012-09-12 09:09:58 +0000

    Add a new per-host report, more data for the per-player report, and clean up the existing reporting system a little.

--- src/ggp/database/reports/HostReport.java
+++ src/ggp/database/reports/HostReport.java
@@ -2,11 +2,12 @@ package ggp.database.reports;
 
 import ggp.database.Persistence;
 import ggp.database.matches.CondensedMatch;
-import ggp.database.statistics.statistic.WeightedAverageStatistic;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.jdo.Query;
 import javax.mail.Transport;
@@ -19,39 +20,44 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.AddressException;
 
-public class PlayerReport {
+public class HostReport {
 	private static double trimNumber(double x) {
 		return Math.round(x*100)/100.0;
 	}
 	
-    public static void generateReportFor(String thePlayer, String toAddress) throws IOException {        
+	private static String getHostHashedPK(String theHost) {
+		if (theHost.toLowerCase().equals("tiltyard")) return "90bd08a7df7b8113a45f1e537c1853c3974006b2";
+		if (theHost.toLowerCase().equals("dresden")) return "f69721b2f73839e513eed991e96824f1af218ac1";
+		if (theHost.toLowerCase().equals("artemis")) return "5bc94f8e793772e8585a444f2fc95d2ac087fed0";
+		if (theHost.toLowerCase().equals("sample")) return "0ca7065b86d7646166d86233f9e23ac47d8320d4";
+		return theHost;
+	}
+	
+    public static void generateReportFor(String theHost, String toAddress) throws IOException {        
         StringBuilder queryFilter = new StringBuilder();
-        queryFilter.append("playerNamesFromHost == '" + thePlayer + "' && ");
+        queryFilter.append("hashedMatchHostPK == '" + getHostHashedPK(theHost) + "' && ");
         queryFilter.append("startTime > " + (System.currentTimeMillis() - 604800000L));
         
         Query query = Persistence.getPersistenceManager().newQuery(CondensedMatch.class);
         query.setFilter(queryFilter.toString());
         
         int nMatches = 0;
-        long latestCleanStartTime = 0;
-        WeightedAverageStatistic.NaiveWeightedAverage myAvgScore = new WeightedAverageStatistic.NaiveWeightedAverage();
-        WeightedAverageStatistic.NaiveWeightedAverage myAvgErrors = new WeightedAverageStatistic.NaiveWeightedAverage();
+        int nAbandonedMatches = 0;
+        Set<String> distinctGames = new HashSet<String>();
+        Set<String> distinctPlayers = new HashSet<String>();
+        long latestStartTime = 0;
         
         try {
             @SuppressWarnings("unchecked")
 			List<CondensedMatch> results = (List<CondensedMatch>) query.execute();
-            for (CondensedMatch e : results) {            	
-            	int nRole = e.playerNamesFromHost.indexOf(thePlayer);
-            	if (!e.isCompleted) continue;
-            	
-            	if (e.hasErrorsForPlayer.get(nRole)) {
-            		myAvgErrors.addEntry(1.0, 1.0);
-            	} else {
-            		myAvgErrors.addEntry(0.0, 1.0);
-            		myAvgScore.addEntry(e.goalValues.get(nRole), 1.0);
-                	if (e.startTime > latestCleanStartTime) {
-                		latestCleanStartTime = e.startTime;
-                	}
+            for (CondensedMatch e : results) {
+            	distinctGames.add(e.gameMetaURL);
+            	distinctPlayers.addAll(e.playerNamesFromHost);
+            	if (e.startTime > latestStartTime) {
+            		latestStartTime = e.startTime;
+            	}
+            	if (!e.isCompleted && e.startTime < System.currentTimeMillis()-21600000L) {
+            		nAbandonedMatches++;
             	}
             	nMatches++;
             }
@@ -60,12 +66,13 @@ public class PlayerReport {
         }
         
         StringBuilder theMessage = new StringBuilder();
-        theMessage.append("Daily activity report for player " + thePlayer + ", generated on " + new Date() + ".\n");
+        theMessage.append("Daily activity report for host " + theHost + ", generated on " + new Date() + ".\n");
         theMessage.append("Counts all activity over the past seven days.\n\n");
         theMessage.append("Total matches: " + nMatches + "\n");
-        theMessage.append("Percentage with errors: " + trimNumber(myAvgErrors.getWeightedAverage()*100) + "%\n");
-        theMessage.append("Avg score, for clean matches: " + trimNumber(myAvgScore.getWeightedAverage()) + "\n");
-        theMessage.append("Last clean match started on " + new Date(latestCleanStartTime) + "\n");
+        theMessage.append("Percentage abandoned: " + trimNumber((nAbandonedMatches*1.0/nMatches)*100) + "%\n");
+        theMessage.append("Unique 7DA players: " + distinctPlayers.size() + "\n");
+        theMessage.append("Unique 7DA games: " + distinctGames.size() + "\n");
+        theMessage.append("Last match started on " + new Date(latestStartTime) + "\n");
         
         Properties props = new Properties();
         Session session = Session.getDefaultInstance(props, null);
@@ -74,7 +81,7 @@ public class PlayerReport {
             Message msg = new MimeMessage(session);
             msg.setFrom(new InternetAddress("noreply-reporting@ggp-database.appspotmail.com", "GGP.org Reporting"));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(toAddress));
-            msg.setSubject("GGP.org Daily Report for player " + thePlayer + " on " + new Date());
+            msg.setSubject("GGP.org Daily Report for host " + theHost + " on " + new Date());
             msg.setText(theMessage.toString());
             Transport.send(msg);
         } catch (AddressException e) {
