6491d49071f36111f3beac876af131433622f82f
Sam Schreiber <sam.schreiber@gmail.com>
2012-12-06 06:14:26 +0000

    Remove the web site attached to database.ggp.org, since it's not used. The database.ggp.org server will be exclusively for machines to access.

--- src/ggp/database/GGP_DatabaseServlet.java
+++ src/ggp/database/GGP_DatabaseServlet.java
@@ -14,10 +14,7 @@ import ggp.database.statistics.MatchStatistics;
 import ggp.database.statistics.NewStatisticsComputation;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
 
@@ -209,66 +206,9 @@ public class GGP_DatabaseServlet extends HttpServlet {
             return;
         }
 
-        boolean writeAsBinary = false;
-        if (reqURI.endsWith("/")) {
-            reqURI += "index.html";
-        }
-        if (reqURI.endsWith(".html")) {
-            resp.setContentType("text/html");
-        } else if (reqURI.endsWith(".xml")) {
-            resp.setContentType("application/xml");
-        } else if (reqURI.endsWith(".xsl")) {
-            resp.setContentType("application/xml");
-        } else if (reqURI.endsWith(".js")) {
-            resp.setContentType("text/javascript");   
-        } else if (reqURI.endsWith(".json")) {
-            resp.setContentType("text/javascript");
-        } else if (reqURI.endsWith(".png")) {
-            resp.setContentType("image/png");
-            writeAsBinary = true;
-        } else if (reqURI.endsWith(".ico")) {
-            resp.setContentType("image/png");
-            writeAsBinary = true;
-        } else {
-            resp.setContentType("text/plain");
-        }
-
-        try {
-            if (writeAsBinary) {
-                writeStaticBinaryPage(resp, reqURI.substring(1));
-            } else {
-                // Temporary limits on caching, for during development.
-                resp.setHeader("Cache-Control", "no-cache");
-                resp.setHeader("Pragma", "no-cache");
-                writeStaticTextPage(resp, reqURI.substring(1));
-            }
-        } catch(IOException e) {
-            resp.setStatus(404);
-        }
+        resp.setStatus(404);
     }
     
-    public void writeStaticTextPage(HttpServletResponse resp, String theURI) throws IOException {
-        FileReader fr = new FileReader(theURI);
-        BufferedReader br = new BufferedReader(fr);
-        StringBuffer response = new StringBuffer();
-        
-        String line;
-        while( (line = br.readLine()) != null ) {
-            response.append(line + "\n");
-        }
-
-        resp.getWriter().println(response.toString());
-    }
-    
-    public static void writeStaticBinaryPage(HttpServletResponse resp, String theURI) throws IOException {
-        InputStream in = new FileInputStream(theURI);
-        byte[] buf = new byte[1024];
-        while (in.read(buf) > 0) {
-            resp.getOutputStream().write(buf);
-        }
-        in.close();        
-    }    
-
     private static final int INGESTION_RETRIES = 10;
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
