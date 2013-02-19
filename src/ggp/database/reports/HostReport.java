a5a1e26854d89756f4304d8b7e6b8bd6f66ef38f
Sam Schreiber <sam.schreiber@gmail.com>
2013-02-19 00:27:50 +0000

    Add fields to the per-host report about player hours consumed and the number of 7DA unique skilled players, defined as players in the top 25% of the scaled agon scores for the host.

--- src/ggp/database/reports/HostReport.java
+++ src/ggp/database/reports/HostReport.java
@@ -1,13 +1,18 @@
 package ggp.database.reports;
 
 import org.ggp.galaxy.shared.persistence.Persistence;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ggp.database.matches.CondensedMatch;
 import ggp.database.statistics.statistic.WeightedAverageStatistic;
+import ggp.database.statistics.stored.FinalOverallStats;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -17,6 +22,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import java.util.Properties;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -35,6 +41,12 @@ public class HostReport {
 		return theHost;
 	}
 	
+	private static Set<String> intersect(Collection<String> a, Collection<String> b) {
+		Set<String> x = new HashSet<String>(a);
+		x.retainAll(b);
+		return x;
+	}
+	
     public static void generateReportFor(String theHost, Collection<String> toAddresses) throws IOException {
     	if (toAddresses.size() == 0) return;
     	
@@ -48,23 +60,43 @@ public class HostReport {
         int nMatches = 0;
         Set<String> distinctGames = new HashSet<String>();
         Set<String> distinctPlayers = new HashSet<String>();
+        Set<String> distinctSkilledPlayers = new HashSet<String>();
         long latestStartTime = 0;
+        double millisecondsPlayed = 0;
         WeightedAverageStatistic.NaiveWeightedAverage playersPerMatch = new WeightedAverageStatistic.NaiveWeightedAverage();
         WeightedAverageStatistic.NaiveWeightedAverage fractionScrambled = new WeightedAverageStatistic.NaiveWeightedAverage();
         WeightedAverageStatistic.NaiveWeightedAverage fractionAbandoned = new WeightedAverageStatistic.NaiveWeightedAverage();
         WeightedAverageStatistic.NaiveWeightedAverage fractionAborted = new WeightedAverageStatistic.NaiveWeightedAverage();
 
+        Set<String> skilledPlayers = new HashSet<String>();
         try {
+	        JSONObject hostStats = FinalOverallStats.load(theHost).getJSON();
+	        JSONObject agonScaledSkill = hostStats.getJSONObject("agonScaledSkill");
+	        Iterator<?> agonSkillItr = agonScaledSkill.keys();
+	        while (agonSkillItr.hasNext()) {
+	        	String player = agonSkillItr.next().toString();
+	        	double skill = agonScaledSkill.getDouble(player);
+	        	if (skill > 0.75) {
+	        		skilledPlayers.add(player);
+	        	}
+	        }
+        } catch (JSONException je) {
+        	throw new RuntimeException(je);
+        }
+        
+		try {
             @SuppressWarnings("unchecked")
 			List<CondensedMatch> results = (List<CondensedMatch>) query.execute();
             for (CondensedMatch e : results) {
             	distinctGames.add(e.gameMetaURL);
             	distinctPlayers.addAll(e.playerNamesFromHost);
+            	distinctSkilledPlayers.addAll(intersect(e.playerNamesFromHost, skilledPlayers));
             	playersPerMatch.addEntry(e.matchRoles, 1.0);
             	fractionScrambled.addEntry((e.scrambled != null && e.scrambled) ? 1 : 0, 1.0);
             	fractionAbandoned.addEntry((!e.isCompleted && (e.isAborted == null || !e.isAborted) && e.startTime < System.currentTimeMillis()-21600000L) ? 1 : 0, 1.0);
             	fractionAborted.addEntry((e.isAborted != null && e.isAborted) ? 1 : 0, 1.0);
             	latestStartTime = Math.max(latestStartTime, e.startTime);
+            	millisecondsPlayed += e.matchLength * e.matchRoles;
             	nMatches++;
             }
         } finally {
@@ -80,8 +112,10 @@ public class HostReport {
         theMessage.append("Percentage aborted: " + trimNumber(fractionAborted.getWeightedAverage()*100) + "%\n");
         theMessage.append("Percentage scrambled: " + trimNumber(fractionScrambled.getWeightedAverage()*100) + "%\n");        
         theMessage.append("Average players/match: " + trimNumber(playersPerMatch.getWeightedAverage()) + "\n");
-        theMessage.append("Unique 7DA players: " + distinctPlayers.size() + "\n");
-        theMessage.append("Unique 7DA games: " + distinctGames.size() + "\n");
+        theMessage.append("Player-hours consumed: " + trimNumber(millisecondsPlayed/3600000.0) + "\n");
+        theMessage.append("Unique 7DA skilled players: " + distinctSkilledPlayers.size() + "\n");
+        theMessage.append("Unique 7DA players: " + distinctPlayers.size() + "\n");        
+        theMessage.append("Unique 7DA games: " + distinctGames.size() + "\n");        
         theMessage.append("Last match started on " + new Date(latestStartTime) + "\n");
         
         Properties props = new Properties();
